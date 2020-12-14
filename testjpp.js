095fea2184cfd85f8f80a4a50c22687bb708c36a
Zark52 <70440865+Zark52@users.noreply.github.com>
2020-12-14 23:13:40 +0100

    Add files via upload

---
+++ testjpp.js
@@ -0,0 +1,5 @@
+function pute() {
+    if (document.getElementById("pute").innerHTML == "")
+        document.getElementById("pute").innerHTML = "Your mom gay"
+        else document.getElementById("pute").innerHTML =""
+} 
